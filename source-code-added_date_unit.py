6b03070e9d08f817ae566272ddb005361b8657e4
abhishek singh <110660543+abhishek986873@users.noreply.github.com>
2023-02-12 22:33:15 +0530

    Update source-code-added_date_unit.py

--- source-code-added_date_unit.py
+++ source-code-added_date_unit.py
@@ -1,4 +1,4 @@
-# import statement
+# mport statement
 import docx as docx
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -15,7 +15,6 @@ repair = 150
 dental_repair = 150
 cd = 600
 zirconia = 1800
-
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -37,7 +36,8 @@ main_heading = cell.add_paragraph("ORO CARE DENTAL LAB\n H.NO- 223, GALI NO.-2, 
 main_heading.style = "Title"
 
 # Add the subheading
-sub_heading = cell.add_paragraph("Ref.........                                                                              Date-31-01-2023 \n\nINSTITUTE OF PARAMEDICAL TECHNOLOGY \nCHATTERPUR , NEW DELHI\n")
+sub_heading = cell.add_paragraph(
+    "Ref.........                                                                              Date-31-01-2023 \n\nINSTITUTE OF PARAMEDICAL TECHNOLOGY \nCHATTERPUR , NEW DELHI\n")
 sub_heading.style = "Subtitle"
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -45,7 +45,8 @@ sub_heading.style = "Subtitle"
 # TAKING INPUT OF NUMBER OF ROW IN A TABLE
 
 bill_row = int(input("ENTER NUMBER OF ROW = "))
-print("------------------------------------------------⚫ UNIT----------------------------------------------------------------------------------------------------")
+print(
+    "------------------------------------------------⚫ UNIT----------------------------------------------------------------------------------------------------")
 
 # ADDING 1 TO ROW VALUE BECAUSE WE HAVE TO CONSIDER THE OTH INDEX THAT IS CONSIST OF HEADING LIKE UNIT , S.NO , DATE
 
@@ -86,29 +87,38 @@ i = 4
 while i < bill_row:
     string = f"ENTER {i}TH UNIT = "
     unit_value_list.append(string)
-    i = i+1
+    i = i + 1
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #  entering the value of unit inside the cell
 
+
+# creating empty list to store value of unit
+
+
+unit_list = []
 i = 1
 j = 0
 while i < bill_row:
     data_row = table.rows[i].cells
-    data_row[2].text = input(unit_value_list[j])
+    unit_value = data_row[2].text = input(unit_value_list[j])
+    unit_list.append(unit_value)
     i = i + 1
     j = j + 1
 
+
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-print("------------------------------------------------⚫ DATE----------------------------------------------------------------------------------------------------")
+print(
+    "------------------------------------------------⚫ DATE----------------------------------------------------------------------------------------------------")
 
 # TAKING INPUT OF MONTH FOR DATE COLOUMN
 
 month = input("ENTER MONTH = ")
 
-print("--------------------------------------------------------------------------------------------------------------------------------------------------")
+print(
+    "--------------------------------------------------------------------------------------------------------------------------------------------------")
 
 # Creating list for greeting the DATE
 
@@ -130,7 +140,8 @@ while i < bill_row:
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-print("------------------------------------------------⚫ WORK----------------------------------------------------------------------------------------------------")
+print(
+    "------------------------------------------------⚫ WORK----------------------------------------------------------------------------------------------------")
 
 # creating greeting for work
 
@@ -143,16 +154,88 @@ while i < bill_row:
 
 # inserting value inside work cell
 
+# inserting the work inside a list
+
+work_list = []
 i = 1
 j = 0
 while i < bill_row:
     data_row = table.rows[i].cells
-    data_row[3].text = input(work_value_list[j])
+    work_value = data_row[3].text = input(work_value_list[j])
+    work_list.append(work_value)
+    i = i + 1
+    j = j + 1
+
+# --------------------------------------AMOUNT--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# creating list of amount value
+
+amount_value_list = []
+i = 0
+j = 1
+while j < bill_row:
+    if work_list[i] == "PFM":
+        amount_value = int(unit_list[i]) * pfm
+        amount_value_list .append(amount_value)
+
+    elif work_list[i] == "DMLS":
+        amount_value = int(unit_list[i]) * dmls
+        amount_value_list .append(amount_value)
+
+    elif work_list[i] == "METAL FREE":
+        amount_value = int(unit_list[i]) * metal_free
+        amount_value_list .append(amount_value)
+
+    elif work_list[i] == "FLEXIBLE":
+        amount_value = int(unit_list[i]) * flexible
+        amount_value_list .append(amount_value)
+
+    elif work_list[i] == "RPD":
+        amount_value = int(unit_list[i]) * rpd
+        amount_value_list .append(amount_value)
+
+    elif work_list[i] == "ORTHO":
+        amount_value = int(unit_list[i]) * ortho
+        amount_value_list .append(amount_value)
+
+    elif work_list[i] == "REPAIR":
+        amount_value = int(unit_list[i]) * repair
+        amount_value_list .append(amount_value)
+
+    elif work_list[i] == "DENTAL REPAIR":
+        amount_value = int(unit_list[i]) * dental_repair
+        amount_value_list .append(amount_value)
+
+    elif work_list[i] == "CD":
+        amount_value = int(unit_list[i]) * cd
+        amount_value_list .append(amount_value)
+
+    elif work_list[i] == "ZIRCONIA":
+        amount_value = int(unit_list[i]) * zirconia
+        amount_value_list .append(amount_value)
+    i = i + 1
+    j = j + 1
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# inserting amount value
+
+
+i = 1
+j = 0
+while i < bill_row:
+    data_row = table.rows[i].cells
+    data_row[4].text = str(amount_value_list[j])
+
     i = i + 1
     j = j + 1
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+# CALCULATING TOTAL AMOUNT
+
+total_amount = sum(amount_value_list)
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# entering the value of total and greeting inside mintu table
 
 
 
@@ -164,120 +247,15 @@ while i < bill_row:
 
 
 
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-table = mintu_bill.add_table(rows= 2 , cols=5)
+table = mintu_bill.add_table(rows=2, cols=5)
+data_row = table.rows[1].cells
+data_row[3].text = "TOTAL"
+data_row = table.rows[1].cells
+data_row[4].text = str(total_amount)
 mintu_bill.save("mintuttt_bill.docx")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# data_row = table.rows[2].cells
+# data_row[4].text = "TOTAL AMOUNT"
